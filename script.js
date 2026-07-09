5d9715a96f2c70c08b361f1a25dd801bc0a17765
Piriwa82 <121771599+Piriwa82@users.noreply.github.com>
2026-07-09 12:35:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.querySelectorAll(".boton-agregar").forEach((boton) => {
 
 // WhatsApp y Total/Ahorro
 // URL de tu Apps Script de Google Sheets para registrar pedidos automáticamente
-const googleSheetsAppURL = "https://script.google.com/macros/s/AKfycbzYlPNkKsE-pb4rk8RUrJIVd42hvkHXYNmWoi_6ro1lmiuLZyEjmKpibG668ESU2UwKYQ/exec";
+const googleSheetsAppURL = "https://script.google.com/macros/s/AKfycbw0vhGaCdP6kojogxsEdu_2mIyrHdfe6GrFMjRuO7N7jdMS7ZStYbqFuV4nLZfK2aEzrA/exec";
 
 // WhatsApp y Total/Ahorro
 const numeroWhatsapp = "543517612075";
